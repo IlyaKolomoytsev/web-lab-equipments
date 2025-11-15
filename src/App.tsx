1bb0e2b80903fbad8608e6335058a39ade80a79f
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-15 19:35:33 +0300

    feat(Home): move content from App to HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';  // Измените на Routes
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';  // Измените на Routes
 import './styles/App.css';
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import {ThemeProvider} from "./utils/ThemeContext";
-import PageThemeToggle from "./components/PageThemeToggle/PageThemeToggle";
-import Header from "./components/Header/Header";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <ThemeProvider>
-                <div>
-                    <Header title={"лох"}></Header>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    {/* Маршруты */}
-                    <Routes>  {/* Заменили Switch на Routes */}
-                        <Route path="/" element={<HomePage/>}/> {/* Заменили component на element */}
-                        <Route path="/about" element={<AboutPage/>}/> {/* Заменили component на element */}
-                    </Routes>
-                </div>
-            </ThemeProvider>
-        </Router>
+        <div className="container">
+            <Router >
+                <ThemeProvider>
+                    <div>
+                        {/* Маршруты */}
+                        <Routes>  {/* Заменили Switch на Routes */}
+                            <Route path="/" element={<HomePage/>}/> {/* Заменили component на element */}
+                            <Route path="/about" element={<AboutPage/>}/> {/* Заменили component на element */}
+                        </Routes>
+                    </div>
+                </ThemeProvider>
+            </Router>
+        </div>
     );
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from "../components/Header/Header.tsx";
+import Button from "../components/Buttons/Button.tsx";
+import {HideIcon, RemoveIcon, ShowIcon} from "../components/Icons/Icons.tsx";
+import GroupCard, {type GroupCardProps} from "../components/cards/GroupCard.tsx";
+
 
 const HomePage: React.FC = () => {
-    return <h1>Welcome to the Home Page!</h1>;
+
+    const [formVisibility, setFormVisibility] = useState(false);
+
+    const data: GroupCardProps[] = [
+        {
+            title: "Микрофоны",
+            description: "оборудование",
+        },
+        {
+            title: "Колонки",
+            description: "палёные",
+        }
+    ];
+
+    const formVisibilityButton = (isOpen: boolean) => {
+        if (isOpen) {
+            return ["Hide", HideIcon()]
+        } else {
+            return ["Show", ShowIcon()]
+        }
+    }
+
+    const changeFormVisibility = () => {
+        setFormVisibility(visibility => !visibility);
+    }
+
+    return <div>
+        <Header title={"Groups of equipments"}></Header>
+        <div className="toolbar">
+            <Button variant="danger">
+                {RemoveIcon()} RemoveAll
+            </Button>
+            <Button variant="secondary" onClick={changeFormVisibility}>
+                {formVisibilityButton(formVisibility)}
+            </Button>
+        </div>
+        <div className="create-form">
+
+        </div>
+        <div className="list">
+            {data.map(item => (
+                <GroupCard {...item} />
+            ))}
+        </div>
+    </div>
 }
 
 export default HomePage;
