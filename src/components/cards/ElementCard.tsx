5989eb6ccaee1b6fe402503f7cfabd4fb839d8e1
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-15 16:52:20 +0300

    feat(Card): move icons form cards to separate file

--- src/components/cards/ElementCard.tsx
+++ src/components/cards/ElementCard.tsx
@@ -1,6 +1,7 @@
 import styles from "./Card.module.css"
 import Button from "../Buttons/Button.tsx";
 import {useState} from "react";
+import {EditIcon, RemoveIcon, RentedIcon, StockItem} from "../Icons/Icons.tsx";
 
 interface CardProps {
     title: string;
@@ -9,41 +10,6 @@ interface CardProps {
     editAction?: () => void;
     removeAction?: () => void;
 }
-
-const EditIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-pencil">
-        <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
-        <path d="m15 5 4 4"/>
-    </svg>
-)
-
-const RemoveIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-2">
-        <path d="M3 6h18"/>
-        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
-        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
-        <line x1="10" x2="10" y1="11" y2="17"/>
-        <line x1="14" x2="14" y1="11" y2="17"/>
-    </svg>
-)
-
-const RentedIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-lock"
-         viewBox="0 0 16 16">
-        <path
-            d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m0 5.996V14H3s-1 0-1-1 1-4 6-4q.845.002 1.544.107a4.5 4.5 0 0 0-.803.918A11 11 0 0 0 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664zM9 13a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1"/>
-    </svg>
-)
-
-const StockItem = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-seam"
-         viewBox="0 0 16 16">
-        <path
-            d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
-    </svg>
-)
 
 const GroupCard: React.FC<CardProps> = (props) => {
     const [rented, setRented] = useState(props.rented);
