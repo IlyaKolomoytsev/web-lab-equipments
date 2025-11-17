e82c6b1de2d83306a31c10ded4b32f4be715e399
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-18 01:15:11 +0300

    feat(isons): introduce back icon

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -46,3 +46,10 @@ export const HideIcon = () => (
         <path d="m18 15-6-6-6 6"/>
     </svg>
 )
+
+export const BackIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-chevron-left">
+        <path d="m15 18-6-6 6-6"/>
+    </svg>
+)
