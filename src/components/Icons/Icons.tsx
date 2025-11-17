6d27560acfd7afd56adc39d0b2140ad1b823ba22
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-18 01:15:15 +0300

    feat(isons): introduce add icon

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -53,3 +53,12 @@ export const BackIcon = () => (
         <path d="m15 18-6-6 6-6"/>
     </svg>
 )
+
+export const AddIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-plus-circle">
+        <circle cx="12" cy="12" r="10"/>
+        <path d="M8 12h8"/>
+        <path d="M12 8v8"/>
+    </svg>
+)
