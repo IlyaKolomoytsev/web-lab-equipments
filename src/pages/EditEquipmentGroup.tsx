08e538e0c8830680c3ece7e48ee097430265f5e8
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-18 15:12:31 +0300

    feat(edit group): add actions for buttons on edit group page

--- src/pages/EditEquipmentGroup.tsx
+++ src/pages/EditEquipmentGroup.tsx
@@ -5,35 +5,41 @@ import {BackIcon, EditIcon} from "../components/Icons/Icons.tsx";
 import {useEquipmentsStore} from "../utils/EquipmentContext.tsx";
 import {useNavigate, useParams} from "react-router-dom";
 import Header from "../components/Header/Header.tsx";
+import type {EquipmentGroup} from "../types/equipments.ts";
 
 const EditEquipmentGroup: React.FC = () => {
     const equipment = useEquipmentsStore()
     const navigate = useNavigate();
-    const { id } = useParams<{ id: string }>();
+    const {id} = useParams<{ id: string }>();
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    let foundEntry: EquipmentGroup | undefined;
+    if (id) {
+        const equipmentId = parseInt(id, 10);
+        foundEntry = equipment.groups.find(group => group.id === equipmentId);
+    }
 
     useEffect(() => {
-        if (id) {
-            const equipmentId = parseInt(id, 10);
-            const foundEntry = equipment.groups.find(group => group.id === equipmentId);
-            if (foundEntry) {
-                setTitle(foundEntry.title);
-                setDescription(foundEntry.description);
-                return;
-            }
+        if (foundEntry) {
+            setTitle(foundEntry.title);
+            setDescription(foundEntry.description);
+            return;
         }
         alert(`Equipment group with id: ${id} not found!`);
         navigate("/"); // Перенаправляем на главную, если запись не найдена
-    }, [id, equipment.groups, navigate]);
+    }, [navigate, foundEntry, id]);
 
-
-
+    if (!foundEntry) {
+        return null;
+    }
     return (
         <div>
             <Header title={"Edit equipment"}></Header>
-            <form className="edit-form">
+            <form className="edit-form"
+                  onSubmit={(e: React.FormEvent) => {
+                      e.preventDefault();
+                  }}>
                 <InputField
                     label="Edit equipment title"
                     placeholder="Edit equipment title"
@@ -47,8 +53,18 @@ const EditEquipmentGroup: React.FC = () => {
                     setContent={setDescription}
                 ></InputField>
                 <div>
-                    <Button variant="primary" style={{marginRight: "0.2rem"}}>{BackIcon()} Back</Button>
-                    <Button variant="primary">{EditIcon()} Edit</Button>
+                    <Button
+                        variant="primary"
+                        onClick={() => navigate("/")}
+                        style={{marginRight: "0.2rem"}}
+                    >{BackIcon()} Back</Button>
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            equipment.editGroup(foundEntry.id, title, description)
+                            alert("Changes saved!")
+                        }}
+                    >{EditIcon()} Edit</Button>
                 </div>
             </form>
 
