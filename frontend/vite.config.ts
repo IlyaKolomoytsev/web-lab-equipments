65127af3d2b054fea4f40a8cdcbbc236ff8be13d
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-11-23 21:57:44 +0300

    fix: vite proxy server for backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
-            '/api': 'http://localhost:5000', // Прокси для API
+            '/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
         },
     },
 })
